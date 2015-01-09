9762132142abcf7f2094e04515c75d38fb1edb2c
Steelchopsticks <ManueleBryan@gmail.com>
2015-01-09 12:38:37 -0800

    Test Commit. Added Intent

--- app/src/main/java/manuele/bryan/androidgithubtest/MainActivity.java
+++ app/src/main/java/manuele/bryan/androidgithubtest/MainActivity.java
@@ -1,5 +1,6 @@
 package manuele.bryan.androidgithubtest;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+
     }
 
 
